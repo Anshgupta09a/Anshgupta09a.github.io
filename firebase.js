3635fab89c4c65d9ce9cd95b3689b051ade136d6
Anshgupta09a <anshraj336676@gmail.com>
2026-07-27 13:57:16 +0530

    Set up Firebase connection in firebase.js
    Initialize Firebase app with configuration settings.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBCvcUugYuYQcK6zVYmlqxyyaBGPHSmlTQ",
+  authDomain: "gupta-cosmetic-shop.firebaseapp.com",
+  databaseURL: "https://gupta-cosmetic-shop-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gupta-cosmetic-shop",
+  storageBucket: "gupta-cosmetic-shop.firebasestorage.app",
+  messagingSenderId: "648214036605",
+  appId: "1:648214036605:web:dc541d3d7e21f5f072b114"
+};
+
+const app = initializeApp(firebaseConfig);
+console.log("Firebase Connected");
